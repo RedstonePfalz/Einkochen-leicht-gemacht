3c3a3192441b5819350dcf7d4f86fcfe0b5552cf
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2023-10-14 11:52:06 +0200

    chore: Add home_md_loader.js to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require("path");
 module.exports = {
     mode: 'production',
     entry: {
-      obst: "./www/js/obst.js"
+      obst: "./www/js/obst.js",
+      home_md_loader: "./www/js/home_md_loader.js"
     },
     output: {
         filename: '[name].min.js', // Verwendet den Dateinamen der Eingabedatei
