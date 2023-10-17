d9682fffeb80b02a42df0b48291ccdfc7048b13d
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2023-10-17 18:28:52 +0200

    breaking change: Update Home-Page to Material 3

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,21 @@
+let pages = ["home", "obst", "gemuese", "tipps", "produkte"];
+
+function observePageChanges() {
+    const targetNode = document.querySelector("body");
+
+    const config = { childList: true, subtree: true };
+
+    const callback = function(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                location.reload();
+            }
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+}
 
 function switchPage(page) {
     $.ajax({
@@ -6,22 +24,39 @@ function switchPage(page) {
             $("#app").html(result);
         }
     });
+
+    document.querySelectorAll(".page_js").forEach(function (element) {
+       element.remove();
+    });
+
     let scriptElement = document.createElement("script");
+    let scriptElement2 = document.createElement("script");
     if (page === "home") {
         scriptElement.src = "js/" + page + ".js";
+        scriptElement2.src = "js/home_md_loader.min.js";
+        scriptElement2.className = "page_js";
+        document.head.appendChild(scriptElement2);
     } else {
         scriptElement.src = "js/" + page + ".min.js";
     }
+    scriptElement.className = "page_js";
     document.head.appendChild(scriptElement);
     localStorage.setItem("page", page);
+    window.location.hash = page;
+    observePageChanges();
 }
+
 
 function onBackButtonPress() {
     switchPage("home");
 }
 
-switchPage("home");
 
+if (pages.includes(window.location.hash.replace('#', ''))) {
+    switchPage(window.location.hash.replace('#', ''));
+} else {
+    switchPage("home");
+}
 
 
 
