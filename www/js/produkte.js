7b8f7117a387185c565626e2986877c92e4b5729
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2023-10-18 14:17:38 +0200

    fix(product-page): Productlink can now open

--- www/js/produkte.js
+++ www/js/produkte.js
@@ -1,6 +1,8 @@
-function openProductPage(link) {
-    window.open("https://redstonepfalz.github.io/elg-web/r.html?id=" + link);
-}
+document.querySelectorAll(".product_item").forEach(function(element) {
+    element.addEventListener("click", function() {
+        window.open("https://redstonepfalz.github.io/elg-web/r.html?id=" + $(element).attr("product_id"), "_blank", "location=yes");
+    });
+});
 
 document.querySelectorAll(".year").forEach(function(element) {
     $(element).text(new Date().getFullYear());
