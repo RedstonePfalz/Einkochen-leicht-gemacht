dc7c2f861c1718e48c232882ac0ac556cc0fd01d
RedstonePfalz <55254344+RedstonePfalz@users.noreply.github.com>
2023-11-11 18:21:37 +0100

    feat(fruit-page): Reduce Sugar quantities

--- www/js/obst.js
+++ www/js/obst.js
@@ -16,32 +16,36 @@ function selectmenuchange() {
     //TODO: Sprachenunterstützung per JSON-Strings
 
     notice.hide();
+    z1l.show();
+    z1k.show();
 
     if (obst === "apfel") {
         zeit.text("30");
         temperatur.text("85");
-        z1k.text("Nicht verfügbar");
-        z1l.text("350-500 g");
+        z1k.text("200-300 g");
+        z1l.text("200-300 g");
+        notice.show();
+        notice.text("Bei Bedarf können die Äpfel vor dem Einmachen auch mit Zimt, Vanille oder Ingwer gewürzt werden.");
     }
 
     if (obst === "apfelmus") {
         zeit.text("30");
         temperatur.text("90");
-        z1l.text("Nicht verfügbar");
-        z1k.text("Nicht verfügbar");
+        z1l.hide();
+        z1k.hide();
     }
 
     if (obst === "aprikosen") {
         zeit.text("30");
         temperatur.text("85");
-        z1l.text("200-400 g");
+        z1l.text("200-350 g");
         z1k.text("Nicht verfügbar");
     }
 
     if (obst === "birnen") {
         zeit.text("30/80");
         temperatur.text("90");
-        z1l.text("400-600 g");
+        z1l.text("250-400 g");
         z1k.text("Nicht verfügbar");
         notice.show();
         notice.text("Wichtig: Weiche Birnen werden 30 min lang eingekocht, harte Birnen 80 min.\n" +
@@ -51,29 +55,29 @@ function selectmenuchange() {
     if (obst === "brombeeren") {
         zeit.text("25");
         temperatur.text("85");
-        z1l.text("500-600 g");
+        z1l.text("350-500 g");
         z1k.text("150-250 g");
     }
 
     if (obst === "erdbeeren") {
         zeit.text("25");
         temperatur.text("80");
-        z1l.text("400-600 g");
+        z1l.text("250-350 g");
         z1k.text("150-200 g");
     }
 
     if (obst === "himbeeren") {
         zeit.text("30");
         temperatur.text("80");
-        z1l.text("500-600 g");
+        z1l.text("300-400 g");
         z1k.text("150-200 g");
     }
 
     if (obst === "johannisbeeren") {
         zeit.text("25");
         temperatur.text("90");
-        z1l.text("600-750 g");
-        z1k.text("250-300 g");
+        z1l.text("500-600 g");
+        z1k.text("200-300 g");
     }
 
     if (obst === "kirschen") {
@@ -83,28 +87,28 @@ function selectmenuchange() {
         z1k.text("Siehe Hinweis");
         notice.show();
         notice.text("WICHTIG: Sauerkirschen: 400-600g Zucker auf 1l Wasser und 300-400g Zucker auf 1kg Obst\n" +
-            " Süßkirschen: 250-450g Zucker auf 1l Wasser und 150-250g Zucker auf 1kg Obst");
+            " Süßkirschen: 250-350g Zucker auf 1l Wasser und 150-200g Zucker auf 1kg Obst");
     }
 
     if (obst === "mirabellen") {
         zeit.text("30");
         temperatur.text("85");
-        z1l.text("300-450 g");
-        z1k.text("Nicht verfügbar");
+        z1l.text("250-300 g");
+        z1k.text("200-250 g");
     }
 
     if (obst === "pfirsiche") {
         zeit.text("30");
         temperatur.text("85");
-        z1l.text("300-500 g");
+        z1l.text("250-400 g");
         z1k.text("Nicht verfügbar");
     }
 
     if (obst === "pflaumen") {
         zeit.text("30");
         temperatur.text("90");
-        z1l.text("500-700 g");
-        z1k.text("400-600 g");
+        z1l.text("300-350 g");
+        z1k.text("300-350 g");
     }
 
     if (obst === "preiselbeeren") {
@@ -117,22 +121,22 @@ function selectmenuchange() {
     if (obst === "renekloden") {
         zeit.text("30");
         temperatur.text("85");
-        z1l.text("300-450 g");
-        z1k.text("Nicht verfügbar");
+        z1l.text("Nicht verfügbar");
+        z1k.text("250 g");
     }
 
     if (obst === "rhabarber") {
         zeit.text("30");
         temperatur.text("95");
-        z1l.text("400-600 g");
-        z1k.text("300-450 g");
+        z1l.text("200-250 g");
+        z1k.text("200-250 g");
     }
 
     if (obst === "stachelbeeren") {
         zeit.text("30");
         temperatur.text("80");
-        z1l.text("750-1000 g");
-        z1k.text("Nicht verfügbar");
+        z1l.text("400-500 g");
+        z1k.text("400-500 g");
     }
 
 
